4934246abb377672aeff2701475db071f7162bcb
Álvaro Molina <abusmolina@gmail.com>
2019-06-17 21:06:09 -0400

    Add new, form, edit show from categories to show form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,21 @@
-class ProductController < ApplicationController
-  
+class ProductsController < ApplicationController
+  # Def
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @product = Product.new(product_params)
+    @product.category = @category
+    if @product.save
+      redirect_to category_path(@category)
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :category_id)
+  end
 end
